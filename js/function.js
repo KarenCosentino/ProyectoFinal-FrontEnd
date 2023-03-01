30f9aea6dd45977fe6d97dd9c03e6e0df2266d05
KarenCosentino <karenluciacosentino@gmail.com>
2023-03-01 10:42:41 -0300

    Septimo commit - Agregando JS al menu

---
+++ js/function.js
@@ -0,0 +1,19 @@
+document.getElementById('nombreButton').addEventListener('click', function() {
+    document.getElementById('infoNombre').style.display = 'block';
+});
+
+document.getElementById('correoButton').addEventListener('click', function() {
+    document.getElementById('infoCorreo').style.display = 'block';
+});
+
+document.getElementById('fechaButton').addEventListener('click', function() {
+    document.getElementById('infoNacimiento').style.display = 'block';
+});
+
+document.getElementById('direccButton').addEventListener('click', function() {
+    document.getElementById('infoDireccion').style.display = 'block';
+});
+
+document.getElementById('telButton').addEventListener('click', function() {
+    document.getElementById('infoTelefono').style.display = 'block';
+});
